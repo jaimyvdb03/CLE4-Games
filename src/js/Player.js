6a55a0a88ea2d313109550251d250ad305577f83
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-06-11 11:07:02 +0200

    Player movement

---
+++ src/js/Player.js
@@ -0,0 +1,48 @@
+import { Actor, Engine, Vector, Keys, CollisionType, DegreeOfFreedom } from "excalibur";
+import { Resources } from './resources.js';
+
+export class Player extends Actor {
+    constructor(x, y) {
+        super({ x, y, width: Resources.Player.width - 5, height: Resources.Player.height });
+        this.body.collisionType = CollisionType.Active; // Active collision type
+    }
+
+    onInitialize(engine) {
+        this.engine = engine;  // Store the engine instance
+        this.body.useGravity = true;
+        this.graphics.use(Resources.Player.toSprite());
+        this.vel = new Vector(0, 0);
+    }
+
+    onPreUpdate(engine, delta) {
+        let xspeed = 0;
+        let yspeed = 0;
+
+        //Player movement
+        //Up
+        if (engine.input.keyboard.isHeld(Keys.W) || engine.input.keyboard.isHeld(Keys.Up)) {
+            yspeed = -350;
+            this.graphics.flipHorizontal = true;
+        }
+
+        //Down
+        if (engine.input.keyboard.isHeld(Keys.S) || engine.input.keyboard.isHeld(Keys.Down)) {
+            yspeed = 350;
+            this.graphics.flipHorizontal = true;
+        }
+
+        //Left
+        if (engine.input.keyboard.isHeld(Keys.A) || engine.input.keyboard.isHeld(Keys.Left)) {
+            xspeed = -350;
+            this.graphics.flipHorizontal = false;
+        }
+
+        //Right
+        if (engine.input.keyboard.isHeld(Keys.D) || engine.input.keyboard.isHeld(Keys.Right)) {
+            xspeed = 350;
+            this.graphics.flipHorizontal = true;
+        }
+
+        this.vel = new Vector(xspeed, yspeed);
+    }
+}
