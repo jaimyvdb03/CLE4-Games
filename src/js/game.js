386bc5af0c5dc673fd6f1bbde6692ee4249d9488
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-06-14 11:56:16 +0200

    game

--- src/js/game.js
+++ src/js/game.js
@@ -25,16 +25,13 @@ export class Game extends Engine {
     startGame() {
         console.log("start de game!");
 
-        
         this.add(new Speedboost(400, 400));
         this.add(new Lifeboost(600, 400));
         this.add(new Lifeboost(630, 400));
         this.add(new Lifeboost(660, 400));
-
-        this.add(new Life(75, 50, player));
-
         let player = new Player(100, 300);
         this.add(player);
+
         //this.camera.zoom = 1.1;
         //this.camera.strategy.lockToActor(player);
         let lich = new Lich(player);
@@ -45,6 +42,9 @@ export class Game extends Engine {
 
         let reaper = new Reaper(player);
         this.add(reaper);
+
+        
+        this.add(new Life(75, 50, player));
 
     }
 }
