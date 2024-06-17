45e323f1fd217909027ca6da483c32f1177d09a9
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-06-17 10:32:41 +0200

    gamepad movement

--- src/js/game.js
+++ src/js/game.js
@@ -1,20 +1,19 @@
-import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
-import { Background } from './background.js'
-import { Player } from './Player.js'
-import { Bush } from './map-objects/bush.js'
-import { Fence } from './map-objects/fence.js'
-import { Bollard } from './map-objects/bollard.js'
-import { Playground } from './map-objects/playground.js'
-import { Hotel } from './hotel.js'
-import { Cockroach } from './cockroach.js'
-import { Reaper } from './reaper.js'
-import { Lich } from './lich.js'
+import '../css/style.css';
+import { Actor, Engine, Vector, DisplayMode, Input } from "excalibur";
+import { Resources, ResourceLoader } from './resources.js';
+import { Background } from './background.js';
+import { Player } from './Player.js';
+import { Bush } from './map-objects/bush.js';
+import { Fence } from './map-objects/fence.js';
+import { Bollard } from './map-objects/bollard.js';
+import { Playground } from './map-objects/playground.js';
+import { Hotel } from './hotel.js';
+import { Cockroach } from './cockroach.js';
+import { Reaper } from './reaper.js';
+import { Lich } from './lich.js';
 import { Speedboost } from './Speedboost.js';
 import { Lifeboost } from './Lifeboost.js';
 import { Life } from './Lifes.js';
-
 
 export class Game extends Engine {
     constructor() {
@@ -23,55 +22,64 @@ export class Game extends Engine {
             height: 720,
             maxFps: 60,
             displayMode: DisplayMode.FitScreen
-        })
-        this.start(ResourceLoader).then(() => this.startGame())
+        });
+        this.start(ResourceLoader).then(() => this.startGame());
     }
 
-    startGame() {
-        console.log("start de game!")
+    // waitForGamepad() {
+    //     console.log("Waiting for gamepad connection...");
 
-        //adding background
+    //     window.addEventListener('gamepadconnected', (event) => {
+    //         console.log('Gamepad connected:', event.gamepad);
+    //         this.startGame(event.gamepad);
+    //     });
+    // }
+
+    startGame(gamepad) {
+        console.log("Start de game!");
+
+        // Adding background
         const background1 = new Background;
         this.add(background1);
 
-        //adding bushes
-        const bush1 = new Bush(375, 380, 190, 210, Resources.Bush1, new Vector(0.5, 1)) //x,y,width,height,image,anchor
+        // Adding bushes
+        const bush1 = new Bush(375, 380, 190, 210, Resources.Bush1, new Vector(0.5, 1)); // x,y,width,height,image,anchor
         this.add(bush1);
-        const bush2 = new Bush(665, 380, 190, 210, Resources.Bush2, new Vector(0.5, 1)) //x,y,width,height,image,anchor
+        const bush2 = new Bush(665, 380, 190, 210, Resources.Bush2, new Vector(0.5, 1)); // x,y,width,height,image,anchor
         this.add(bush2);
-        const bush3 = new Bush(1220, 0, 940, 50, Resources.Bush3, new Vector(1, 0)) //x,y,width,height,image,anchor
+        const bush3 = new Bush(1220, 0, 940, 50, Resources.Bush3, new Vector(1, 0)); // x,y,width,height,image,anchor
         this.add(bush3);
 
-        //adding fence
-        const fence = new Fence()
+        // Adding fence
+        const fence = new Fence();
         this.add(fence);
 
-        //adding bollards
-        const Bollard1 = new Bollard(105, 90, Resources.Bollard1) //x,y,image
+        // Adding bollards
+        const Bollard1 = new Bollard(105, 90, Resources.Bollard1); // x,y,image
         this.add(Bollard1);
-        const Bollard2 = new Bollard(115, 260, Resources.Bollard2) //x,y,height,image
+        const Bollard2 = new Bollard(115, 260, Resources.Bollard2); // x,y,height,image
         this.add(Bollard2);
-        const Bollard3 = new Bollard(115, 470, Resources.Bollard3) //x,y,height,image
+        const Bollard3 = new Bollard(115, 470, Resources.Bollard3); // x,y,height,image
         this.add(Bollard3);
 
-        //adding playground
-        const playground = new Playground()
+        // Adding playground
+        const playground = new Playground();
         this.add(playground);
 
-        //adding hotel
-        const hotel = new Hotel()
+        // Adding hotel
+        const hotel = new Hotel();
         this.add(hotel);
-      
-      this.add(new Speedboost(400, 400));
+
+        this.add(new Speedboost(400, 400));
         this.add(new Lifeboost(600, 400));
         this.add(new Lifeboost(630, 400));
         this.add(new Lifeboost(660, 400));
-      
-        let player = new Player(1350, 300);
+
+        let player = new Player(1350, 300, gamepad);
         this.add(player);
 
-        //this.camera.zoom = 1.1;
-        //this.camera.strategy.lockToActor(player);
+        // this.camera.zoom = 1.1;
+        // this.camera.strategy.lockToActor(player);
         let lich = new Lich(player);
         this.add(lich);
 
@@ -81,9 +89,7 @@ export class Game extends Engine {
         let reaper = new Reaper(player);
         this.add(reaper);
 
-        
         this.add(new Life(75, 50, player));
-
     }
 }
 
