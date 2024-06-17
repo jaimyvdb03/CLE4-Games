5a67b5b7af1205210d51ba4f8c56754bfa656b79
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-06-17 10:54:32 +0200

    Scene 1

--- src/js/game.js
+++ src/js/game.js
@@ -1,13 +1,7 @@
 import '../css/style.css'
 import { Actor, Engine, Vector, DisplayMode } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
-import { Player } from './Player.js'
-import { Cockroach } from './cockroach.js'
-import { Reaper } from './reaper.js'
-import { Lich } from './lich.js'
-import { Speedboost } from './Speedboost.js';
-import { Lifeboost } from './Lifeboost.js';
-import { Life } from './Lifes.js';
+import { Wave1 } from './scene_level1.js';
 
 
 export class Game extends Engine {
@@ -25,26 +19,10 @@ export class Game extends Engine {
     startGame() {
         console.log("start de game!");
 
-        this.add(new Speedboost(400, 400));
-        this.add(new Lifeboost(600, 400));
-        this.add(new Lifeboost(630, 400));
-        this.add(new Lifeboost(660, 400));
-        let player = new Player(100, 300);
-        this.add(player);
+        this.add('wave1', new Wave1());
 
-        //this.camera.zoom = 1.1;
-        //this.camera.strategy.lockToActor(player);
-        let lich = new Lich(player);
-        this.add(lich);
-
-        let roach = new Cockroach(player);
-        this.add(roach);
-
-        let reaper = new Reaper(player);
-        this.add(reaper);
-
-        
-        this.add(new Life(75, 50, player));
+        // Start with the intro scene
+        this.goToScene('wave1');
 
     }
 }
