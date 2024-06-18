62d33c47d7f8609caa695fefe7725c0bd2e2340c
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-06-18 14:09:15 +0200

    player

--- src/js/game.js
+++ src/js/game.js
@@ -17,15 +17,6 @@ export class Game extends Engine {
 
     }
 
-    waitForGamepad() {
-        console.log("Waiting for gamepad connection...");
-
-        window.addEventListener('gamepadconnected', (event) => {
-            console.log('Gamepad connected:', event.gamepad);
-            this.startGame(event.gamepad);
-        });
-    }
-
     addPoints(addedPoints) {
         this.points += addedPoints;
         const currentScene = this.currentScene;
@@ -38,19 +29,18 @@ export class Game extends Engine {
 
    //game.js naar scene 1
    startGame() {
-    console.log("Start de game!");
+        console.log("Start de game!");
 
-    this.add('wave1', new Wave1());
-    // Start with the intro scene
-    this.goToScene('wave1');
+        this.add('wave1', new Wave1());
+        // Start with the intro scene
+        this.goToScene('wave1');
 
-    this.input.gamepads.enabled = true
-    this.input.gamepads.on('connect', (connectevent) => {
-        console.log("gamepad detected")
-        this.mygamepad = connectevent.gamepad
-    })
-
-}
+        this.input.gamepads.enabled = true
+        this.input.gamepads.on('connect', (connectevent) => {
+            console.log("gamepad detected")
+            this.mygamepad = connectevent.gamepad
+        })
+    }
 }
 
 //overige functie moeten naar scene 1 & camara lock op speler
